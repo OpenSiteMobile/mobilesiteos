69190fee61e6b2ed0e88e220f012edcb3f5b0f07
OpenSiteMobile <dwightvietzke@gmail.com>
2014-06-24 12:59:38 -0400

    Change path to site.js, for alt versions

--- config.js
+++ config.js
@@ -25,6 +25,7 @@ msos.console.time('config');
 
 // Set specific flags for this configuration:
 msos.config.run_ads = true;
+msos.config.run_size = true;
 msos.config.run_analytics = true;
 //msos.config.run_onerror = true;
 msos.config.run_social = true;
@@ -68,8 +69,7 @@ if (msos.config.debug_script) {
 		msos.resource_url('underscore', 'v160.uc.js'),
 		msos.resource_url('hammer', 'v106.uc.js'),			// jQuery.hammer.js version of Hammer.js
 		msos.resource_url('backbone', 'v110.uc.js'),
-		msos.resource_url('marionette', 'v123.uc.js'),
-		msos.resource_url('msos', 'site.uc.js'),			// Common installation specific setup code (which needs jQuery, underscore.js, etc.)
+		msos.resource_url('','site.js'),					// Common installation specific setup code (which needs jQuery, underscore.js, etc.)
 		msos.resource_url('msos', 'core.uc.js')
 	];
 
@@ -81,8 +81,8 @@ if (msos.config.debug_script) {
 
 	// Standard site provided (including ext. bundles) scripts
     msos.deferred_scripts = [
-		msos.resource_url('msos', 'bundle.min.js'),			// Modernizr, jQuery, jQuery-UI, Hammer.js, Underscore, Backbone, Marionette bundled together
-		msos.resource_url('msos', 'site.min.js'),
+		msos.resource_url('msos', 'bundle.min.js'),			// Modernizr, jQuery, jQuery-UI, Hammer.js, Underscore, Backbone bundled together
+		msos.resource_url('','site.js'),
 		msos.resource_url('msos', 'core.min.js')
 	];
 
