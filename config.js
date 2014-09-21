4ac5b4997ac108c07f54f68b571683f15e923199
OpenSiteMobile <dwightvietzke@gmail.com>
2014-09-21 12:40:30 -0400

    Upgrade Modernizr, jQuery, Underscore, Hammer.js, Backbone

--- config.js
+++ config.js
@@ -43,7 +43,7 @@ if (msos.config.debug_css) {
 		msos.resource_url('css', 'fawe.css'),
 		msos.resource_url('css', 'msos.css'),
 		msos.resource_url('css', 'msos_bs.css'),
-		msos.resource_url('css', 'msos_theme.css'),
+		msos.resource_url('css', 'msos_theme.css')
 	];
 
 } else {
@@ -63,19 +63,15 @@ if (msos.config.debug_script) {
 
 	// Debug full scripts (line no's mean something)
     msos.deferred_scripts = [
-		msos.resource_url('modernizr', 'v271.uc.js'),		// no class selectors - see build.txt note in /htdocs/modernizr
-		msos.resource_url('jquery', 'v210.uc.js'),
+		msos.resource_url('modernizr', 'v283.uc.js'),		// no class selectors - see build.txt note in /htdocs/modernizr
+		msos.resource_url('jquery', 'v211.uc.js'),
 		msos.resource_url('jquery', 'ui/v1104.uc.js'),		// All UI Core + Draggable Interaction + Effects Core
-		msos.resource_url('underscore', 'v160.uc.js'),
-		msos.resource_url('hammer', 'v106.uc.js'),			// jQuery.hammer.js version of Hammer.js
-		msos.resource_url('backbone', 'v110.uc.js'),
+		msos.resource_url('underscore', 'v170.uc.js'),
+		msos.resource_url('hammer', 'v203.uc.js'),			// jQuery.hammer.js version of Hammer.js
+		msos.resource_url('backbone', 'v112.uc.js'),
 		msos.resource_url('','site.js'),					// Common installation specific setup code (which needs jQuery, underscore.js, etc.)
 		msos.resource_url('msos', 'core.uc.js')
 	];
-
-	if (!msos.config.json) {
-		msos.deferred_scripts.push(msos.resource_url('utils', 'json2.uc.js'));
-	}
 
 } else {
 
@@ -85,10 +81,6 @@ if (msos.config.debug_script) {
 		msos.resource_url('','site.js'),
 		msos.resource_url('msos', 'core.min.js')
 	];
-
-	if (!msos.config.json) {
-		msos.deferred_scripts.push(msos.resource_url('utils', 'json2.min.js'));
-	}
 }
 
 
