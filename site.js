e099e30278614958590cc0b3391388ab811057a3
Dwight Vietzke <dwight_vietzke@yahoo.com>
2015-11-10 08:53:05 -0500

    Isolate addthis variable

--- site.js
+++ site.js
@@ -294,7 +294,7 @@ msos.site.addthis_share = function () {
 
 			atl.add_resource_onload.push(
 				function () {
-					if (addthis && addthis.init) {
+					if (window.addthis) {
 						setTimeout(addthis.init, 250);		// Give AddThis css a chance to load completely
 					} else {
 						msos.console.warn('msos.site.addthis_share -> failed to load!');
