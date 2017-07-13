c269bb843236d7d85f1adb36398d43caa186f58d
Dwight Vietzke <dwight_vietzke@yahoo.com>
2017-07-13 17:14:20 -0400

    Add test facebook api id

--- config.js
+++ config.js
@@ -115,7 +115,7 @@ msos.config.google.hide_tooltip = {
 // Social website API access keys
 msos.config.oauth2 = {
 	google: '296183405068-3379jn2v9polk5aj6j6bilf1k42j0vp2.apps.googleusercontent.com',
-	facebook: '583738878406494',
+	facebook: '169235526952969',
 	windows: '000000004C107945',
 	instagram: '34e2fb9bd305446cb080d852597584e9',
 	cloudmade: 'efca0172cf084708a66a6d48ae1046dd',
